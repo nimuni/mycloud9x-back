2a04423c56da7f3fc3db57d982e1a0096d025f23
nimuni <kym9788@gmail.com>
2023-02-27 21:32:36 +0900

    feat: Mongoose 기본 CRU 구현. Service, ServiceImpl까지

--- service/impl/userServiceImpl.js
+++ service/impl/userServiceImpl.js
@@ -6,16 +6,16 @@ exports.insertOne = async (userObj) => {
 }
 
 exports.findAll = async (findObj, projectionUserObj) => {
-  const users = await User.find({...findObj}).select(projectionUserObj);
+  const users = await User.find(findObj).select(projectionUserObj);
   return users;
 }
 
 exports.findOne = async (findObj, projectionUserObj) => {
-  return await User.findOne({...findObj}).select(projectionUserObj);
+  return await User.findOne(findObj).select(projectionUserObj);
 }
 
 exports.findOneAndUpdate = async (findObj, changeObj, projectionUserObj) => {
-  return await User.findOneAndUpdate({...findObj}, {...changeObj}, {returnOriginal:false}).select(projectionUserObj)
+  return await User.findOneAndUpdate(findObj, changeObj, {returnOriginal:false}).select(projectionUserObj)
 }
 exports.findOneAndDelete = async (findObj) => {
   return await User.findOneAndDelete(findObj);
